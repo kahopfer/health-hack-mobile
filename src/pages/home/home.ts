973f7005e59baa3cc5e7488ff1932c5da313bf1f
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2018-04-07 11:47:31 -0500

    Access camera roll

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {CameraPage} from "../camera/camera";
 import {LoadingPage} from "../loading/loading";
+import {Camera} from "@ionic-native/camera";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit {
-  constructor(public navCtrl: NavController) {
+  imageSrc: string = '';
+  constructor(public navCtrl: NavController,
+              private camera: Camera) {
   }
 
   public ngOnInit(): void {
@@ -20,5 +23,21 @@ export class HomePage implements OnInit {
 
   goToLoading(): void {
     this.navCtrl.push(LoadingPage);
+  }
+
+  openGallery (): void {
+    let cameraOptions = {
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.camera.EncodingType.JPEG,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(cameraOptions)
+      .then(file_uri => this.imageSrc = file_uri,
+        err => console.log(err));
   }
 }
