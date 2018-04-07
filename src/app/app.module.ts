1aa80e0f5d893b943b5de97c0e6466c5af374655
Chau <ctch5@mail.umsl.edu>
2018-04-07 16:08:54 -0500

    Updated NSwag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,58 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {ErrorHandler, NgModule} from '@angular/core';
-import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
-import {SplashScreen} from '@ionic-native/splash-screen';
-import {StatusBar} from '@ionic-native/status-bar';
-
-import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
-import {CameraPage} from "../pages/camera/camera";
-import {CameraPageModule} from "../pages/camera/camera.module";
-import {Camera} from "@ionic-native/camera";
-import {LoadingPage} from "../pages/loading/loading";
-import {LoadingPageModule} from "../pages/loading/loading.module";
-import {ResultsPage} from "../pages/results/results";
-import {ResultsPageModule} from "../pages/results/results.module";
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { Camera } from '@ionic-native/camera';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { CameraPage } from '../pages/camera/camera';
+import { CameraPageModule } from '../pages/camera/camera.module';
+import { HomePage } from '../pages/home/home';
+import { LoadingPage } from '../pages/loading/loading';
+import { LoadingPageModule } from '../pages/loading/loading.module';
+import { ResultsPage } from '../pages/results/results';
+import { ResultsPageModule } from '../pages/results/results.module';
 import { BackendProvider } from '../providers/backend/backend';
-import {HttpClient, HttpClientModule, HttpHandler} from "@angular/common/http";
-import {HttpModule} from "@angular/http";
 import { ImageProvider } from '../providers/image/image';
+import { API_BASE_URL } from './app.api';
+
+import { MyApp } from './app.component';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    CameraPageModule,
-    LoadingPageModule,
-    HttpClientModule,
-    ResultsPageModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    CameraPage,
-    LoadingPage,
-    ResultsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    Camera,
-    BackendProvider,
-    ImageProvider,
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        CameraPageModule,
+        LoadingPageModule,
+        HttpClientModule,
+        ResultsPageModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        CameraPage,
+        LoadingPage,
+        ResultsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        {provide: API_BASE_URL, useFactory: baseUrl},
+        Camera,
+        BackendProvider,
+        ImageProvider,
+    ]
 })
 export class AppModule {
+}
+
+export function baseUrl(): string {
+    return `https://hhstl2018-stl.herokuapp.com/api`;
 }
