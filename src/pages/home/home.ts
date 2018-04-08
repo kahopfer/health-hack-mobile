5c2f9d90e279c26394c7bba66990ccfcf24b2956
Charles Reusnow <Charles.J.Reusnow@centene.com>
2018-04-07 22:37:48 -0500

    Some stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,6 +28,7 @@ export class HomePage implements OnInit {
       quality: 1,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
+      saveToPhotoAlbum: true,
       mediaType: this.camera.MediaType.PICTURE
     };
 
--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -16,7 +16,8 @@ export class ResultsPage implements OnInit {
   descriptionRoot = 'DescriptionPage';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.visionResponse = this.navParams.data.visionResponse;
+    // this.visionResponse = this.navParams.data.visionResponse;
+    this.visionResponse = new VisionResponse({text: "This is some text, this is some more text. This is even more text. Dear god this piano music sucks, please change it, please god. I need more text give me more text.", labels: []})
   }
 
   public ngOnInit(): void {
